e9a68ddc09cf725050e98f4a4c54ca39a67bae6f
Patrick Kaifosh <patrick.kaifosh@mail.corp>
2012-04-04 17:28:13 -0400

    Added a minor correction to gc_all_terms.m

--- code/gc_all_terms.m
+++ code/gc_all_terms.m
@@ -15,10 +15,10 @@ function [all_terms_calc,all_terms_meas] = gc_all_terms(e,m,p)
 
     unival  = @(x,y) (x^3 + 3*x^2*y + 3*x*y^2);
     bival   = @(x,y,z) (3*x^2*y + 3*x*y^2 + 6*x*y*z);
-    trival  = @(x,y,z) x*y*z;
+    trival  = @(x,y,z) 6*x*y*z;
 
     all_terms_calc      = [unival(e,n), unival(m,n), unival(p,n) ...
                         bival(e,m,n), bival(e,p,n), bival(p,m,n) ...
-                        trival(e,m,p), trival(n,n,n)];
+                        trival(e,m,p), n^3];
     
 end
