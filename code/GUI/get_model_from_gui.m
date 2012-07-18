b37e6e7c1aa0938754040de2bd50ba1f570c35a2
Ann Kennedy <annkennedy05@gmail.com>
2012-07-18 19:42:17 -0400

    small fix in the gui

--- code/GUI/get_model_from_gui.m
+++ code/GUI/get_model_from_gui.m
@@ -10,7 +10,6 @@ for i=1:3
     GC_model.MF_input(i)    = get(uictrls.synapse_numbers{i},'Value')-1;
 end
 GC_model.tau_s = get(uictrls.tau_syn_slider,'Value');
-GC_model.tau_s_ratio = get(uictrls.tau_syn_ratio,'Value');
 GC_model.tau_m = get(uictrls.tau_mem_slider,'Value');
 GC_model.GC_to_model=get(uictrls.GC_to_use,'Value');
 GC_model.spiking_on = get(uictrls.spikes,'Value');
